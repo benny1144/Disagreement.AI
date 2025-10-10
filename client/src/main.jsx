042d51e5de30095dbf3b9ad5e92864c70625ca07
benny1144 <lacrockpot@gmail.com>
2025-10-10 00:34:43 -0400

    1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ChakraProvider value={defaultSystem}>
-      <App />
-    </ChakraProvider>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 )
